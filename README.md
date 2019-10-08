10004fe51d1b835273225c87ad852db71d4b9b0d
Nelson Wong <37653189+nwon0002@users.noreply.github.com>
2019-10-08 17:39:00 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Hearts-Game
-Programming Paradigms Assignment 2
+# FIT2102 Assignment 2: Hearts
+
+https://docs.google.com/document/d/10c4XxIA3JmWaoNqG-sShMNqGSjBzNvtFRl3z97PtN5o/edit?usp=sharing
---
+++ staticgame/Player.hs
@@ -0,0 +1,17 @@
+-- | Write a report describing your design and strategy here.
+module Player (
+    playCard,
+    makeBid
+)
+where
+
+-- You can add more imports as you need them.
+import Hearts.Types
+
+
+playCard :: PlayFunc
+playCard = error "You need to implement the playCard function."
+
+-- | Not used, do not remove.
+makeBid :: BidFunc
+makeBid = undefined
